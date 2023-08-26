e3619c46feabe9af042c4d6da1d517e32d17eaa4
Francis Carlo Manlangit <francismanlangit13@gmail.com>
2023-08-26 21:02:16 +0800

    first commit

---
+++ initialize.php
@@ -0,0 +1,16 @@
+<?php
+    if (session_status() !== PHP_SESSION_ACTIVE) {
+        session_start();
+    }
+
+    // Sets default timezome to +8 GMT
+    date_default_timezone_set("Asia/Manila");
+
+    // This is global hosting configuration.
+    if(!defined('date')) define('date', date("Y-m-d h:i:s"));
+    if(!defined('base_url')) define('base_url','http://localhost/rpmsystem/');
+    if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
+    if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
+    if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
+    if(!defined('DB_NAME')) define('DB_NAME',"ssgsystem");
+?>
