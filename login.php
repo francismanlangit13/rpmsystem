26b050794ffe9808c3f28b684ab10bab6179c6ff
Francis Carlo Manlangit <francismanlangit13@gmail.com>
2023-08-26 23:11:14 +0800

    added picture system logo on login page

--- login.php
+++ login.php
@@ -36,7 +36,7 @@
                             <!-- Nested Row within Card Body -->
                             <div class="row">
                                 <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                                <div class="col-lg-6">
+                                <div class="col-lg-6 bg-light">
                                     <div class="p-5">
                                         <div class="text-center">
                                             <h1 class="h4 text-gray-900">Rental Properties Mamangement System</h1>
