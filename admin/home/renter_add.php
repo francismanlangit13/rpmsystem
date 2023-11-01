c271158db8eff6df4170eff0fd5d65cbf1d05232
Francis Carlo Manlangit <francismanlangit13@gmail.com>
2023-11-01 21:53:16 +0800

    add renter user

---
+++ admin/home/renter_add.php
@@ -0,0 +1,101 @@
+<?php include ('../includes/header.php'); ?>
+<style type="text/css">
+    #datatablesSimple th:nth-child(7) {
+        width: 15% !important;
+    }
+</style>
+<main>
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">Add Renter</h1>
+        <ol class="breadcrumb mb-4 mt-3">
+            <li class="breadcrumb-item active"><a href="./home" class="text-decoration-none">Dashboard</a></li>
+            <li class="breadcrumb-item active"><a href="./renter" class="text-decoration-none">Renters</a></li>
+            <li class="breadcrumb-item">Add Renter</li>
+        </ol>
+        <form action="renter_code.php" method="post" autocomplete="off" enctype="multipart/form-data">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Renter form
+                                <div class="float-end">
+                                    <button type="submit" name="add_renter" class="btn btn-primary"><i class="fas fa-plus"></i> Add</button>
+                                </div>
+                            </h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-3 mb-3">
+                                    <label for="fname" class="required">First Name</label>
+                                    <input type="text" class="form-control" placeholder="Enter First Name" name="fname" id="fname" required>
+                                    <div id="fname-error"></div>
+                                </div>
+
+                                <div class="col-md-3 mb-3">
+                                    <label for="mname">Middle Name</label>
+                                    <input type="text" class="form-control" placeholder="Enter Middle Name" name="mname" id="mname">
+                                    <div id="mname-error"></div>
+                                </div>
+
+                                <div class="col-md-3 mb-3">
+                                    <label for="lname" class="required">Last Name</label>
+                                    <input type="text" class="form-control" placeholder="Enter Last Name" name="lname" id="lname" required>
+                                    <div id="lname-error"></div>
+                                </div>
+
+                                <!-- <div class="col-md-3 mb-3">
+                                    <div class="form-group">
+                                        <label for="suffix">Suffix</label>
+                                        <select class="form-control" name="suffix">
+                                            <option value="" selected disabled>Select Suffix</option>
+                                            <option value="Jr">Jr</option>
+                                            <option value="Sr">Sr</option>
+                                            <option value="I">I</option>
+                                            <option value="II">II</option>
+                                            <option value="III">III</option>
+                                            <option value="IV">IV</option>
+                                            <option value="V">V</option>
+                                            <option value="VI">VI</option>
+                                        </select>
+                                        <div id="suffix-error"></div>
+                                    </div>
+                                </div> -->
+
+                                <div class="col-md-3 mb-3">
+                                    <div class="form-group">
+                                        <label for="gender" class="required">Gender</label>
+                                        <select class="form-control" name="gender" id="gender" required>
+                                            <option value="" selected disabled>Select Gender</option>
+                                            <option value="Male">Male</option>
+                                            <option value="Female">Female</option>
+                                        </select>
+                                        <div id="gender-error"></div>
+                                    </div>
+                                </div>
+    
+                                <div class="col-md-4 mb-3">
+                                    <label for="email" class="required">Email</label>
+                                    <input type="email" class="form-control" placeholder="Enter Email" name="email" id="email" required>
+                                    <div id="email-error"></div>
+                                </div>
+
+                                <div class="col-md-4 mb-3">
+                                    <label for="phone" class="required">Phone Number</label>
+                                    <input type="text" class="form-control" placeholder="Enter Phone" name="phone" maxlength="11" id="phone" required>
+                                    <div id="phone-error"></div>
+                                </div>
+
+                                <div class="col-md-4 mb-3">
+                                    <label for="password" class="required">Password</label>
+                                    <input type="password" class="form-control" placeholder="Enter Password" name="password" id="password" required>
+                                    <div id="password-error"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</main>
+<?php include ('../includes/bottom.php'); ?>
