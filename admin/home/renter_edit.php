3c02506f872a5a65cab125e46efcfdf979f48ddf
Francis Carlo Manlangit <francismanlangit13@gmail.com>
2023-11-01 21:54:46 +0800

    add renter edit

---
+++ admin/home/renter_edit.php
@@ -0,0 +1,132 @@
+<?php include ('../includes/header.php'); ?>
+<style type="text/css">
+    #datatablesSimple th:nth-child(7) {
+        width: 15% !important;
+    }
+</style>
+<main>
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">Edit Renter</h1>
+        <ol class="breadcrumb mb-4 mt-3">
+            <li class="breadcrumb-item active"><a href="./home" class="text-decoration-none">Dashboard</a></li>
+            <li class="breadcrumb-item active"><a href="./renter" class="text-decoration-none">Renters</a></li>
+            <li class="breadcrumb-item">Edit Renter</li>
+        </ol>
+        <?php
+            if(isset($_GET['id'])) {
+                $id = $_GET['id'];
+                $sql = "SELECT * FROM `user` WHERE `user_id` = '$id' AND `status` != 'Archive' AND `type` = 'Renter'";
+                $sql_run = mysqli_query($con, $sql);
+
+                if(mysqli_num_rows($sql_run) > 0) {
+                    foreach($sql_run as $row){
+        ?>
+        <form action="renter_code.php" method="post" autocomplete="off" enctype="multipart/form-data">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Renter form
+                                <div class="float-end">
+                                    <button type="submit" name="edit_renter" class="btn btn-success"><i class="fas fa-save"></i> Save</button>
+                                    <input type="hidden" name="user_id" value="<?=$row['user_id']?>">
+                                </div>
+                            </h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-3 mb-3">
+                                    <label for="fname" class="required">First Name</label>
+                                    <input type="text" class="form-control" placeholder="Enter First Name" name="fname" id="fname" value="<?=$row['fname']?>" required>
+                                    <div id="fname-error"></div>
+                                </div>
+
+                                <div class="col-md-3 mb-3">
+                                    <label for="mname">Middle Name</label>
+                                    <input type="text" class="form-control" placeholder="Enter Middle Name" name="mname" id="mname" value="<?=$row['mname']?>">
+                                    <div id="mname-error"></div>
+                                </div>
+
+                                <div class="col-md-3 mb-3">
+                                    <label for="lname" class="required">Last Name</label>
+                                    <input type="text" class="form-control" placeholder="Enter Last Name" name="lname" id="lname" value="<?=$row['lname']?>" required>
+                                    <div id="lname-error"></div>
+                                </div>
+
+                                <!-- <div class="col-md-3 mb-3">
+                                    <div class="form-group">
+                                        <label for="suffix">Suffix</label>
+                                        <select class="form-control" id="suffix" name="suffix">
+                                            <option value="" selected>Select Suffix</option>
+                                            <option value="" <?= isset($row['suffix']) && $row['suffix'] == '' ? 'selected' : '' ?>>None</option>
+                                            <option value="Jr" <?= isset($row['suffix']) && $row['suffix'] == 'Jr' ? 'selected' : '' ?>>Jr</option>
+                                            <option value="Sr" <?= isset($row['suffix']) && $row['suffix'] == 'Sr' ? 'selected' : '' ?>>Sr</option>
+                                            <option value="I" <?= isset($row['suffix']) && $row['suffix'] == 'I' ? 'selected' : '' ?>>I</option>
+                                            <option value="II" <?= isset($row['suffix']) && $row['suffix'] == 'II' ? 'selected' : '' ?>>II</option>
+                                            <option value="III" <?= isset($row['suffix']) && $row['suffix'] == 'III' ? 'selected' : '' ?>>III</option>
+                                            <option value="IV" <?= isset($row['suffix']) && $row['suffix'] == 'IV' ? 'selected' : '' ?>>IV</option>
+                                            <option value="V" <?= isset($row['suffix']) && $row['suffix'] == 'V' ? 'selected' : '' ?>>V</option>
+                                            <option value="VI" <?= isset($row['suffix']) && $row['suffix'] == 'VI' ? 'selected' : '' ?>>VI</option>
+                                        </select>
+                                        <div id="suffix-error"></div>
+                                    </div>
+                                </div> -->
+
+                                <div class="col-md-3 mb-3">
+                                    <div class="form-group">
+                                        <label for="gender" class="required">Gender</label>
+                                        <select class="form-control" name="gender" id="gender" required>
+                                            <option value="" selected disabled>Select Gender</option>
+                                            <option value="Male" <?= isset($row['gender']) && $row['gender'] == 'Male' ? 'selected' : '' ?>>Male</option>
+                                            <option value="Female" <?= isset($row['gender']) && $row['gender'] == 'Female' ? 'selected' : '' ?>>Female</option>
+                                        </select>
+                                        <div id="gender-error"></div>
+                                    </div>
+                                </div>
+    
+                                <div class="col-md-4 mb-3">
+                                    <label for="email" class="required">Email</label>
+                                    <input type="email" class="form-control" placeholder="Enter Email" name="email" id="email" value="<?=$row['email']?>" required>
+                                    <div id="email-error"></div>
+                                </div>
+
+                                <div class="col-md-4 mb-3">
+                                    <label for="phone" class="required">Phone Number</label>
+                                    <input type="text" class="form-control" placeholder="Enter Phone" name="phone" maxlength="11" id="phone" value="<?=$row['phone']?>" required>
+                                    <div id="phone-error"></div>
+                                </div>
+
+                                <div class="col-md-4 mb-3">
+                                    <div class="form-group">
+                                        <label for="status" class="required">Status</label>
+                                        <select class="form-control" name="status" id="status" required>
+                                            <option value="" selected disabled>Select Role</option>
+                                            <option value="Active" <?= isset($row['status']) && $row['status'] == 'Active' ? 'selected' : '' ?>>Active</option>
+                                            <option value="Inactive" <?= isset($row['status']) && $row['status'] == 'Inactive' ? 'selected' : '' ?>>Inactive</option>
+                                        </select>
+                                        <div id="status-error"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+        <?php } } else{ ?>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Renter info</h4>
+                        </div>
+                        <div class="card-body">
+                            <h4>No records found.</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php } } ?>
+    </div>
+</main>
+<?php include ('../includes/bottom.php'); ?>
