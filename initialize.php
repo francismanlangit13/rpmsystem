d3783dd6c01fc4758c724d3514153fc9d7cb485a
Francis Carlo A. Manlangit <francismanlangit13@gmail.com>
2023-12-12 10:41:01 -0800

    fix issue

--- initialize.php
+++ initialize.php
@@ -13,7 +13,7 @@
     if(!defined('emailuser')) define('emailuser', 'host.sendmailer@gmail.com'); // Email for GoogleAPI
     if(!defined('emailpass')) define('emailpass', 'lzqrmanuoigtudvt'); // Password for GoogleAPI
     if(!defined('smsapiname')) define('smsapiname', 'SEMAPHORE'); // API SMS sender name
-    if(!defined('smsapikey')) define('smsapikey', ''); // API SMS KEY
+    if(!defined('smsapikey')) define('smsapikey', '4a98784e7c3a64890dfcb1cc9183f3ad'); // API SMS KEY
     if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
     if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
     if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
